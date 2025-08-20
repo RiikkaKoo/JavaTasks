96ce3c44a07ed7ea25a9079efebeab9be02a2050
riikka <riikaka@metropolia.fi>
2025-08-20 11:33:00 +0300

    Task 1.3.2: Binary values done

---
+++ Moduuli1.3/src/BinaryValues.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BinaryValues {
+    public static void main (String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Give a binary number: ");
+        String bin = userInput.nextLine();
+        int dec = 0;
+        int p = 0;
+        for (int i = bin.length() - 1; i >= 0; i--) {
+            char bit = bin.charAt(i);
+            if (bit == '1') {
+                dec += (int)Math.pow(2, p);
+            }
+            p++;
+        }
+        System.out.println("In decimal: " + dec);
+    }
+}
