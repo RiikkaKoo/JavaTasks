f2d41fa8d0c1b61ae0bc7a8cafa1c1a1ee36212d
riikka <riikaka@metropolia.fi>
2025-08-24 15:44:18 +0300

    Made the prints cleaner

--- Moduuli1.3/src/PrimeNumbers.java
+++ Moduuli1.3/src/PrimeNumbers.java
@@ -16,13 +16,14 @@ public class PrimeNumbers {
             System.out.println("Give an ending number that is larger than the starting number: ");
             end = userInput.nextInt();
         } while (end <= start);
+        System.out.println("Here are the prime numbers between the two given numbers: ");
         for (int i = start; i <= end; i++) { // Go through all the numbers from start to end (i)
             for (int i2 = 2; i2 <= i; i2++) { // Check if the current number can be divided with another smaller number (i2)
                 if (i%i2 == 0 && i2 < i) { // If so, it is not a prime number  --> Stop checking
                     break;
                 }
                 else if (i%i2 == 0 && i2 == i) { // If not, it is a prime numbers --> Print it
-                    System.out.print(i + "\n");
+                    System.out.print(i + "  ");
                 }
             }
         }
